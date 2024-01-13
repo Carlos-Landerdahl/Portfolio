38ee225239bcbe9c06f40ca025300f0d839bdd89
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2024-01-13 20:19:47 -0300

    feat: improve responsive screen

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,13 +5,13 @@ import SendButton from './sendEmail'
 export default function Header() {
   return (
     <nav className="px-2 border-b border-[--border-2] sm:px-3 fixed z-[100] bg-[--bg-black] w-full">
-      <div className="h-[60px] max-w-7xl gap-2 m-auto flex px-1 items-center bg-transparent text-[--text-hover]">
-        <section className="flex justify-center w-full items-center sm:justify-normal">
+      <div className="h-[60px] max-w-7xl justify-center gap-2 m-auto flex px-1 items-center bg-transparent text-[--text-hover]">
+        <section className="flex pr-2  items-center sm:justify-normal sm:w-full">
           <Link href="/" className="hover:animate-spin ease-in-out">
             <ChevronRightSquare size={25} />
           </Link>
         </section>
-        <section className="flex justify-between w-full gap-3 items-center sm:justify-end">
+        <section className="flex justify-center gap-3 items-center sm:justify-end">
           <ul className="flex gap-6 text-sm font-bold">
             <li className="text-[--header-link] hover:text-[--text-hover] transition ease-in-out ">
               <Link href="/">Sobre</Link>
