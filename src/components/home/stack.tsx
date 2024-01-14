582d15f6877f1f6dfde4c4880428d142b34b7a96
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2024-01-14 17:37:51 -0300

    feat: improve colors

--- src/components/home/stack.tsx
+++ src/components/home/stack.tsx
@@ -10,37 +10,43 @@ export default function Stack() {
       </h2>
       <div className="flex gap-3 w-full flex-wrap justify-center md:justify-normal">
         <Tooltip content="Next.js" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
-            <Image src="/next.svg" alt="" width={70} height={70} />
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
+            <Image
+              src="/next.svg"
+              alt=""
+              width={70}
+              height={70}
+              className="hover:fill-black fill-current"
+            />
           </div>
         </Tooltip>
         <Tooltip content="React.js" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/react.svg" alt="" width={50} height={50} />
           </div>
         </Tooltip>
         <Tooltip content="React Query" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/react-query.svg" alt="" width={50} height={50} />
           </div>
         </Tooltip>
         <Tooltip content="Typescript" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/types.svg" alt="" width={40} height={40} />
           </div>
         </Tooltip>
         <Tooltip content="Java" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/java.svg" alt="" width={50} height={50} />
           </div>
         </Tooltip>
         <Tooltip content="Spring Boot" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/spring.svg" alt="" width={50} height={50} />
           </div>
         </Tooltip>
         <Tooltip content="Docker" className="bg-[--bg-card] text-white">
-          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-white transition-all ease-linear">
+          <div className="bg-[--bg-button] w-[120px] h-[70px] rounded flex flex-col justify-center items-center hover:bg-[--hover-card] transition-all ease-linear">
             <Image src="/docker.svg" alt="" width={50} height={50} />
           </div>
         </Tooltip>
